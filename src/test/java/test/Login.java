5083c5018a97b733ad0ba1f4cab7aa6eac7615cd
Ramesh A <-ramesharpu@gmail.com>
2024-01-29 16:27:49 +0530

    added invalid tc with login functionality

--- src/test/java/test/Login.java
+++ src/test/java/test/Login.java
@@ -23,4 +23,22 @@ public class Login extends BaseTest{
 		driver.findElement(By.id("email")).sendKeys(username);
 		driver.findElement(By.id("pass")).sendKeys(password);
 	}
+	
+	@Test
+	public void invalidUsernamInvalidPasswordTest() {
+		String username = "invalid-dan@gmail.com";
+		String password = "invlaid-test1234";
+
+		driver.findElement(By.id("email")).sendKeys(username);
+		driver.findElement(By.id("pass")).sendKeys(password);
+	}
+	
+	@Test
+	public void validUsernamInvalidPasswordTest() {
+		String username = "dan@gmail.com";
+		String password = "invlaid-test1234";
+
+		driver.findElement(By.id("email")).sendKeys(username);
+		driver.findElement(By.id("pass")).sendKeys(password);
+	}
 }
